5777449dd6a055bbdc1ba1e392e403f7a00de2c2
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-14 15:08:01 -0300

    Criado a página de usuarios

--- site_dona_maca/app_cad_usuario/views.py
+++ site_dona_maca/app_cad_usuario/views.py
@@ -5,4 +5,12 @@ def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
-    pass
+    novo_usuario = Usuarios()
+    #Salvar os usuários da tela para o banco de dados
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    return render(request, 'usuarios/usuarios.html', usuarios)
