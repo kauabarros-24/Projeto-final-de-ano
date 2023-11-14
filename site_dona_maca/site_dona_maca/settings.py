57d814c5bdcb26d04761640be17e7f3be38259c8
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-14 14:33:14 -0300

    front-end feito

--- site_dona_maca/site_dona_maca/settings.py
+++ site_dona_maca/site_dona_maca/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_cad_usuarios'
+    'app_cad_usuario'
 ]
 
 MIDDLEWARE = [
