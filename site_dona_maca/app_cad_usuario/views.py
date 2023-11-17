55577794473d94d16e5f771acc0222b911e5c4dc
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-17 16:06:52 -0300

    Reiniciamos o trabalho

--- site_dona_maca/app_cad_usuario/views.py
+++ site_dona_maca/app_cad_usuario/views.py
@@ -1,16 +1,5 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'usuarios/home.html')
-
-def usuarios(request):
-    novo_usuario = Usuarios()
-    #Salvar os usuários da tela para o banco de dados
-    novo_usuario.nome = request.POST.get('nome')
-    novo_usuario.idade = request.POST.get('idade')
-    novo_usuario.save()
-    usuarios = {
-        'usuarios': Usuario.objects.all()
-    }
-    return render(request, 'usuarios/usuarios.html', usuarios)
+    return render(request,'usuarios/home.html')
     
