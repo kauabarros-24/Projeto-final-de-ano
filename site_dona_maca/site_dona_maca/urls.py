0b545756294970d2c5e0a7a1c7f6000cdd829170
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-20 14:12:34 -0300

    2

--- site_dona_maca/site_dona_maca/urls.py
+++ site_dona_maca/site_dona_maca/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     #Rota, view responsável, nome de referência
     #Ex: Facebook.com
     path('', views.home, name='home'),
+    # usuarios.com
+    path('usuarios/', views.usuarios, name = 'listagem_usuarios')
     
    
    
