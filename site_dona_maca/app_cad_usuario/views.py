721644ff29558037cb63755bfbe1b950cd5c8294
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-20 14:29:31 -0300

    Para garantir segurança

--- site_dona_maca/app_cad_usuario/views.py
+++ site_dona_maca/app_cad_usuario/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Usuarios
 
 def home(request):
     return render(request,'usuarios/home.html')
     
 def usuarios(request):
-    pass
+    #Salvar os dados da tela para o banco
+    novo_usuario = Usuarios()
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.save()
+    #Exibir os usuários cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuarios.objects.all()
+    }
+    #Retornar para a página de listagem
+    return render(request, 'usuarios/usuarios.html')
