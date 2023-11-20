42bb385cc35161cc911716ce4b6b9aee794ac693
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-20 13:52:03 -0300

    Estrutura html refeita

--- site_dona_maca/site_dona_maca/settings.py
+++ site_dona_maca/site_dona_maca/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_cad_usuario'
-]
+] 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
