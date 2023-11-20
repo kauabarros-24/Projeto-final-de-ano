4a2320b3d3e0f9c16e4cab4aa4f96cb77f29851a
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-20 15:20:02 -0300

    Novos dados

--- site_dona_maca/app_cad_usuario/admin.py
+++ site_dona_maca/app_cad_usuario/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+
+from app_cad_usuario.models import Usuarios
+
+admin.site.register(Usuarios)
--- site_dona_maca/app_cad_usuario/models.py
+++ site_dona_maca/app_cad_usuario/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Usuarios(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=255)
-    idade = models.IntegerField()
+    idade = models.IntegerField(blank=True, null=True)
