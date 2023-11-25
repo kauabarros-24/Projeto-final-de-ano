838fbfb2bb275ad67d0a9e9b15992f5f73ea5cbb
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-25 18:34:52 -0300

    Alterada de Usuarios()  para NovoDados()

--- site_dona_maca/app_cad_usuario/models.py
+++ site_dona_maca/app_cad_usuario/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 
-class Usuarios(models.Model):
+
+class NovosDados(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     email = models.EmailField(max_length=255)
     senha = models.TextField(max_length=255)
--- site_dona_maca/app_cad_usuario/views.py
+++ site_dona_maca/app_cad_usuario/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from .models import Usuarios
+from .models import NovosDados
 
 def home(request):
     return render(request,'usuarios/home.html')
 
 def usuarios(request):
     #Salvar os usuarios
-    novo_usuario = Usuarios()
+    novo_usuario = NovosDados()
     novo_usuario.email = request.POST.get('email')
     novo_usuario.senha = request.POST.get('senha')
     novo_usuario.save()
     #Exibir todos os usuários
     usuarios = {
-        'usuarios': Usuarios.objects.all()
+        'usuarios': NovosDados.objects.all()
     }
     #Retornar os dados para a página de listagem de usuários
     return render(request, 'usuarios/home.html', usuarios)
