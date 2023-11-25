dba49ecca63629fd41a5a5f974d9a7adc220eba9
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-25 16:14:59 -0300

    Novos dados

--- site_dona_maca/app_cad_usuario/models.py
+++ site_dona_maca/app_cad_usuario/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 class Usuarios(models.Model):
-    id_usuario = models.AutoField(primary_key= True)
-    nome = models.TextField(max_length=255)
-    idade = models.IntegerField()
+    id_usuario = models.AutoField(primary_key=True)
+    email = models.TextField(max_length=255)
+    senha = models.TextField(max_length=255)
+    
--- site_dona_maca/app_cad_usuario/views.py
+++ site_dona_maca/app_cad_usuario/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'usuarios/home.html')
+
+def usuarios(request):
+    pass
+
     
