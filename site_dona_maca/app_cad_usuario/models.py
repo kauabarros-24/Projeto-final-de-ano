9bca85f5d087c6f7c14d3ba8e5e98091bf4b9b59
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-25 18:27:43 -0300

    textField de email para emailField

--- site_dona_maca/app_cad_usuario/models.py
+++ site_dona_maca/app_cad_usuario/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Usuarios(models.Model):
     id_usuario = models.AutoField(primary_key=True)
-    email = models.TextField(max_length=255)
+    email = models.EmailField(max_length=255)
     senha = models.TextField(max_length=255)
     
