a84a93753150ec98f8e88af011a5c820bee0f6d4
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-11-25 16:27:29 -0300

    Recebendo dados do usuário

--- site_dona_maca/app_cad_usuario/views.py
+++ site_dona_maca/app_cad_usuario/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from .models import Usuarios
 
 def home(request):
     return render(request,'usuarios/home.html')
 
 def usuarios(request):
-    pass
+    #Salvar os usuarios
+    novo_usuario = Usuarios()
+    novo_usuario.email = request.POST.get('email')
+    novo_usuario.senha = request.POST.get('senha')
+    novo_usuario.save()
+    #Exibir todos os usuários
+    usuarios = {
+        'usuarios': Usuarios.objects.all()
+    }
+    #Retornar os dados para a página de listagem de usuários
+    return render(request, 'usuarios/index.html', usuarios)
+    
 
     
