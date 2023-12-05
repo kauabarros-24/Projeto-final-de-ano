5590cadb634fcc581be5b8ef2b7c7681471f0e78
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-05 09:33:45 -0300

    Criamos um novo projeto

--- site_dona_maca/app_cad_usuario/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-
-class NovosDados(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=255)
-    senha = models.TextField(max_length=255)
-    
