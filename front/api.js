fa267a2c2dd5a5c8849bcf86e29bb51d7b898ca6
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-05 16:17:34 -0300

    f

--- front/api.js
+++ front/api.js
@@ -1,37 +1,39 @@
 
-document.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let senha = document.getElementById('entradaSenha').value;
-    let email = document.getElementById('entradaEmail').value;
+document.addEventListener('submit', () => {
+    const senha = document.getElementById('entradaSenha').value;
+    const email = document.getElementById('entradaEmail').value;
     getUsers()
     addUser()
 })
 
 
-  const data = {
-        email: email,
-        senha: senha,
-    }
+const data = {
+    email: email,
+    senha: senha,
+}
 
-    async function addUser() {
-        const response = await fetch('http://localhost:8000/users/', {
-            method: "POST",
-            headers: {
-                "Conten t-Type": "application/json",
-            },
-            body: JSON.stringify(data)
-        })
-        console.log("Olá");
-        return response.json()
-    }
 
-    async function getUsers() {
-        const response = await fetch('http://localhost:8000/users/')
-        const users = await response.json()
-        document.write(JSON.stringify(users))
-    }
+async function addUser() {
+    const response = await fetch('http://localhost:8000/users/', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    })
+    console.log("Olá");
+    return response.json()
+}
 
-    getUsers()
-    addUser()
+async function getUsers() {
+    const response = await fetch('http://localhost:8000/users/')
+    const users = await response.json()
+    document.write(JSON.stringify(users))
+}
+
+getUsers()
+addUser()
+
+
 
 
