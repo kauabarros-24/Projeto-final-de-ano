90bc0e5c83887c4ac6c23b8b1b4ae0ec1b4bd1b4
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-12-05 15:39:53 -0300

    Api do cavalo

--- front/api.js
+++ front/api.js
@@ -1,37 +1,37 @@
 
-document.addEventListener('submit', async (event) => {
+document.addEventListener('submit', (event) => {
     event.preventDefault();
-    
     let senha = document.getElementById('entradaSenha').value;
     let email = document.getElementById('entradaEmail').value;
+    getUsers()
+    addUser()
+})
 
-    const data = {
+
+  const data = {
         email: email,
         senha: senha,
-    };
-
-    try {
-        await addUser();
-        await getUsers();
-    } catch (error) {
-        console.error("Erro ao adicionar usuário:", error);
     }
-});
 
-async function addUser() {
-    const response = await fetch('http://localhost:8000/users/', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    });
+    async function addUser() {
+        const response = await fetch('http://localhost:8000/users/', {
+            method: "POST",
+            headers: {
+                "Conten t-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        console.log("Olá");
+        return response.json()
+    }
 
-    return response.json();
-}
+    async function getUsers() {
+        const response = await fetch('http://localhost:8000/users/')
+        const users = await response.json()
+        document.write(JSON.stringify(users))
+    }
 
-async function getUsers() {
-    const response = await fetch('http://localhost:8000/users/');
-    const users = await response.json();
-    document.write(JSON.stringify(users));
-}
+    getUsers()
+    addUser()
+
+
